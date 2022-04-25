36f9148a3b3d3b66f0df29bf21da8b5024e2fa11
Tahseen <tahseenislam@hotmail.com>
2022-04-25 19:53:38 +1000

    added cool colorways

--- src/components/ColorPane.tsx
+++ src/components/ColorPane.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import { IndividualColor } from './ui/IndividualColor'
+import React from 'react';
+import { IndividualColor } from './ui/IndividualColor';
 
 export const ColorPane = () => {
   return (
     <ul className='color-pane--wrapper'>
-      <IndividualColor color={'red'} />
-      <IndividualColor color={'green'} />
-      <IndividualColor color={'blue'} />
-      <IndividualColor color={'orange'} />
-      <IndividualColor color={'yellow'} />
-      <IndividualColor color={'purple'} />
+      <IndividualColor color={'#ff6d5b'} />
+      <IndividualColor color={'#e6b905'} />
+      <IndividualColor color={'#6fd262'} />
+      <IndividualColor color={'#ea86c2'} />
+      <IndividualColor color={'#c78eff'} />
+      <IndividualColor color={'#5ac0e7'} />
+      <IndividualColor color={'#aaaaaa'} />
     </ul>
-  )
-}
+  );
+};
